3543d0b463aaa710d917d811c825acc8096e57b7
filip <fbielejec@gmail.com>
2016-05-20 14:22:25 +0200

    handlers for resources

--- src/main/java/oscilloscope/Oscilloscope.java
+++ src/main/java/oscilloscope/Oscilloscope.java
@@ -1,63 +1,71 @@
 package oscilloscope;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 
 import resources.JSONResource;
-import resources.Resource;
 
 /**
  * @fbielejec
  */
 
-
-//ServletContextHandler sch = new ServletContextHandler();
-//sch.setContextPath("/xxx");
-//
-//resource = new TheResource();
-//ResourceConfig rc = new ResourceConfig();
-//rc.register(resource);
-//
-//ServletContainer sc = new ServletContainer(rc);        
-//ServletHolder holder = new ServletHolder(sc);
-//sch.addServlet(holder, "/*");
-//
-//Server server = new Server(port);
-//server.setHandler(sch);
-//server.start();
-//server.join();
-
 public class Oscilloscope {
-
-	// http://www.nikgrozev.org/2014/10/16/rest-with-embedded-jetty-and-jersey-in-a-single-jar-step-by-step/
 
 	public static void main(String[] args) {
 
-        Server jettyServer = new Server(8080);
-        
- 
-     	ServletHolder servletHolder = new ServletHolder("default", new DefaultServlet());
-//     	ServletHolder servletHolder = new ServletHolder(ServletContainer.class, "/*");
-        
-        // Tells the Jersey Servlet which REST service/class to load.
-        servletHolder.setInitParameter(
-           "jersey.config.server.provider.classnames",
-           JSONResource.class.getCanonicalName());
+		Server jettyServer = new Server(8080);
 
-      
-      
-      
-        servletHolder.setInitParameter("resourceBase", "./src/webapp/");
+		////////////////////////
+		// ---JSON RESOURCE---//
+		////////////////////////
+
+		JSONResource resource = new JSONResource();
 		
-		ServletContextHandler context = new ServletContextHandler();
+		ResourceConfig rc = new ResourceConfig();
+		rc.register(resource);
+
+		ServletContainer sc = new ServletContainer(rc);
+
+		ServletHolder servletHolder = new ServletHolder(sc);
+
+		ServletContextHandler jsonResourceContext = new ServletContextHandler();
+		jsonResourceContext.addServlet(servletHolder, "/*");
+
+		//////////////////////////
+		// ---STATIC RESOURCE---//
+		//////////////////////////
+
+		ResourceHandler staticResourceHandler = new ResourceHandler();
+
+		staticResourceHandler.setResourceBase("./src/webapp/");
+
+		//  enable Directory Listing
+//		resourceHandler.setDirectoriesListed(true);
+
+		ContextHandler staticContextHandler = new ContextHandler("/");
+		staticContextHandler.setHandler(staticResourceHandler);
+
+		///////////////////////
+		// ---ADD HANDLERS---//
+		///////////////////////
 		
-        context.addServlet(servletHolder, "/*");
-        
-        jettyServer.setHandler(context);
-        
+		HandlerList handlers = new HandlerList();
+
+		handlers.setHandlers(new Handler[] { jsonResourceContext, //
+				staticContextHandler, //
+				new DefaultHandler() //
+		});
+
+		jettyServer.setHandler(handlers);
+
 		try {
 
 			jettyServer.start();

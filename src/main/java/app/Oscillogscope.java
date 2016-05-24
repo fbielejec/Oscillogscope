6d0deb595be5d3e0cf6357496372e834f1ce5736
filip <fbielejec@gmail.com>
2016-05-24 16:04:32 +0200

    deploying

--- src/main/java/app/Oscillogscope.java
+++ src/main/java/app/Oscillogscope.java
@@ -34,11 +34,14 @@ public class Oscillogscope {
 	}// END: main
 	
     static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
+/**
+ * @return default port if heroku-port isn't set (i.e. on localhost)
+ * */
+    	ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        return 8080; //return default port if heroku-port isn't set (i.e. on localhost)
+        return 8080; 
     }
 	
 }
